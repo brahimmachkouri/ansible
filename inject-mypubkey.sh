be775eaf688304dc7fd646aab340cd24543f9cf4
Brahim Machkouri <brahimmachkouri@users.noreply.github.com>
2022-02-18 18:10:07 +0100

    Add files via upload

---
+++ inject-mypubkey.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "I need the instance name."
+    exit
+fi
+
+INSTANCE_NAME=$1
+multipass copy-files $INSTANCE_NAME:/home/ubuntu/.ssh/authorized_keys .
+cat ~/.ssh/id_rsa.pub >> authorized_keys
+multipass copy-files authorized_keys $INSTANCE_NAME:/home/ubuntu/.ssh/authorized_keys
+echo "Result :"
+multipass exec dev -- cat /home/ubuntu/.ssh/authorized_keys
